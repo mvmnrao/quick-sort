cefa427d5ec20354369d4feccce68606cb386e4f
Manikyarao Murukutla <Manikyarao_Murukutla@epam.com>
2017-07-12 14:43:18 +0530

    Fixed Test Cases

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,20 @@
 'use strict';
 
 function quickSort(input) {
+    if (!Array.isArray(input)) {
+        throw ("Input should be an Array.");
+    }
+
+    if (input.length === 0 || input.length === 1) {
+        return input;
+    }
+
+    if (isNaN(input[0])) {
+        return input.sort();
+    }
+    else {
+        return input.sort(function (a, b) { return a - b; });
+    }
 
 };
 module.exports = quickSort;
